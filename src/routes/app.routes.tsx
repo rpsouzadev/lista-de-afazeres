3c5c1a996038d27111d754775e1ba6f10ffee765
Rafael Souza <rpsouza.dev@gmail.com>
2023-11-10 01:53:39 -0300

    screen home fixed

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,6 +1,6 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 
-import { Home } from '../pages/Home'
+import { Home } from '../pages/home'
 import { NewTask } from '../pages/NewTask'
 import { EditTask } from '../pages/editTask'
 import { DefaultLayout } from '../layouts/DefaultLayout'
